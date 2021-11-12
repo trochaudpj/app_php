cfdde4ff61fa17f9f0e184d1d1db68bb47c1a3a8
trochaudpj <pjtggl@gmail.com>
2021-11-12 14:11:31 +0100

    changement organisation fichiers

--- catalog.php
+++ catalog.php
@@ -1,37 +1,5 @@
-    <?php include 'template/header.php'; ?>
-    <div class="wrapper">
-        <div class="formA">
-            <h1>catalogue</h1>
-            <?php
-            if (isset($_SESSION['message']) || !empty($_SESSION['message'])) {
-                echo '<h2 id="message">' . $_SESSION['message'] . '</h2>';
-                $_SESSION['message'] = '';
-            }
-
-            if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
-                echo "<p>Aucun produit en magasin</p>";
-            } else {
-                echo "<table id='recap'>",
-                "<thead>",
-                "<tr>",
-                "<th>#</th>",
-                "<th>Nom</th>",
-                "<th>Prix</th>",
-                "</tr>",
-                "</thead>",
-                "<tbody>";
-                foreach ($_SESSION['products'] as $index => $product) {
-                    echo "<tr>",
-                    "<td>" . $index . "</td>",
-                    "<td>" . $product['name'] . "</td>",
-                    "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
-                    "</tr>";
-                }
-                echo "</tbody>",
-                "</table>";
-            }
-
-            ?>
-        </div>
-    </div>
+    <?php
+    include 'template/header.php';?>
+    <div class="wrapper"><?php include 'parts/catalog.php';?></div>
+    
     <?php include 'template/footer.php'; ?>
---
+++ parts/catalog.php
@@ -0,0 +1,37 @@
+
+   
+        <div class="formA">
+            <h1>catalogue</h1>
+            <?php
+            if (isset($_SESSION['message']) || !empty($_SESSION['message'])) {
+                echo '<h2 id="message">' . $_SESSION['message'] . '</h2>';
+                $_SESSION['message'] = '';
+            }
+
+            if (!isset($_SESSION['products']) || empty($_SESSION['products'])) {
+                echo "<p>Aucun produit en magasin</p>";
+            } else {
+                echo "<table id='recap'>",
+                "<thead>",
+                "<tr>",
+                "<th>#</th>",
+                "<th>Nom</th>",
+                "<th>Prix</th>",
+                "</tr>",
+                "</thead>",
+                "<tbody>";
+                foreach ($_SESSION['products'] as $index => $product) {
+                    echo "<tr>",
+                    "<td><a href='traitement.php?index=$index&todo=del' class='picto-item' aria-label='suprimer le produit'  ><i class='far fa-trash-alt'></i></a>" . $index . "</td>",
+                    "<td>" . $product['name'] . "</td>",
+                    "<td>" . number_format($product['price'], 2, ",", "&nbsp;") . "&nbsp;€</td>",
+                    "</tr>";
+                }
+                echo "</tbody>",
+                "</table>";
+            }
+
+            ?>
+        </div>
+ 
+  
